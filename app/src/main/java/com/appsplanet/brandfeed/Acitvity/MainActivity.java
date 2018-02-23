06d3e55934fa2d4bc349ce33b2d208a3d54a5196
dell <informidevice@gmail.com>
2018-02-23 12:07:41 +0530

    changes in main screen

--- app/src/main/java/com/appsplanet/brandfeed/Acitvity/MainActivity.java
+++ app/src/main/java/com/appsplanet/brandfeed/Acitvity/MainActivity.java
@@ -213,22 +213,22 @@ public class MainActivity extends AppCompatActivity {
                     String message = null;
                     if (volleyError instanceof NetworkError) {
                         message = "Cannot connect to Internet...Please check your connection!";
-                        Toast.makeText(MainActivity.this, "message", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
                     } else if (volleyError instanceof ServerError) {
                         message = "The server could not be found. Please try again after some time!!";
-                        Toast.makeText(MainActivity.this, "message", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
                     } else if (volleyError instanceof AuthFailureError) {
                         message = "Cannot connect to Internet...Please check your connection!";
-                        Toast.makeText(MainActivity.this, "message", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
                     } else if (volleyError instanceof ParseError) {
                         message = "Parsing error! Please try again after some time!!";
-                        Toast.makeText(MainActivity.this, "message", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
                     } else if (volleyError instanceof NoConnectionError) {
                         message = "Cannot connect to Internet...Please check your connection!";
-                        Toast.makeText(MainActivity.this, "message", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
                     } else if (volleyError instanceof TimeoutError) {
                         message = "Connection TimeOut! Please check your internet connection.";
-                        Toast.makeText(MainActivity.this, "message", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
                     }
 
                 }
